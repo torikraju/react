bc67f344689e665e2c1ed366ecbf38ed7f3c710e
torikraju <torikraju@hotmail.com>
2018-09-02 11:08:15 +0600

    Understanding & Using State

--- 01.Base Features & Syntax/react-first-prictise-project/src/App.js
+++ 01.Base Features & Syntax/react-first-prictise-project/src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+    state = {
+        Persons: [
+            {name: 'Triss', age: 28},
+            {name: 'Yennefer', age: 30},
+            {name: 'Cirilla', age: 22}
+        ]
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>Hello i am react app</h1>
+                <h1>Hello i'm react app</h1>
+                <p>This is really working</p>
+                <button>Show full name</button>
+                <Person name={this.state.Persons[0].name} age={this.state.Persons[0].age}/>
+                <Person name={this.state.Persons[1].name} age={this.state.Persons[1].age}>Powerful sorceress</Person>
+                <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age}/>
             </div>
         );
         //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this works'));
