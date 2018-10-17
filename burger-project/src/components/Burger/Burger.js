1824538579abd0070b9d834c26206bd91622b51c
torikraju <torikraju@hotmail.com>
2018-10-17 18:58:23 +0600

    113 Calculating the Ingredient Sum Dynamically

--- burger-project/src/components/Burger/Burger.js
+++ burger-project/src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import AppUtil from '../../Helper/AppUtil'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredient key={AppUtil.ID()} type={ingredientKey}/>
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients</p>
+    }
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type='bread-top'/>
