86305d0ddce295bb7f90cb1702951a1f271bfeb9
torikraju <torikraju@hotmail.com>
2018-10-17 17:06:36 +0600

    109 Adding a Dynamic Ingredient Component

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import styles from './App.module.css';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
