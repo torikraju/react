318136c9efc0c7402036e6f1f8f5e9748ce38bec
torikraju <torikraju@hotmail.com>
2018-10-17 18:16:47 +0600

    111 Starting the Burger Component

--- burger-project/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-project/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,46 +1,44 @@
 import React, {Component} from 'react';
-import styles from './BurgerIngredient.css'
+import styles from './BurgerIngredient.module.css'
 import PropTypes from 'prop-types';
 
 
 class BurgerIngredient extends Component {
-    render() {
+    render () {
         let ingredient = null;
 
-        switch (this.props.type) {
-            case ('bread-bottom'):
+        switch ( this.props.type ) {
+            case ( 'bread-bottom' ):
                 ingredient = <div className={styles.BreadBottom}></div>;
                 break;
-            case ('bread-top'):
+            case ( 'bread-top' ):
                 ingredient = (
-                    <div className={styles.BreadTop}>
-                        <div className={styles.Seeds1}></div>
-                        <div className={styles.Seeds2}></div>
-                    </div>
+                    <div className={styles.BreadTop}></div>
                 );
                 break;
-            case ('meat'):
+            case ( 'meat' ):
                 ingredient = <div className={styles.Meat}></div>;
                 break;
-            case ('cheese'):
+            case ( 'cheese' ):
                 ingredient = <div className={styles.Cheese}></div>;
                 break;
-            case ('bacon'):
+            case ( 'bacon' ):
                 ingredient = <div className={styles.Bacon}></div>;
                 break;
-            case ('salad'):
+            case ( 'salad' ):
                 ingredient = <div className={styles.Salad}></div>;
                 break;
             default:
                 ingredient = null;
         }
+
         return ingredient;
     }
 }
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
-}
+};
 
 
 export default BurgerIngredient;
