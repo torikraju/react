456a6003c17186395858a004a25b6fae7f99889d
torikraju <torikraju@hotmail.com>
2018-10-18 21:55:23 +0600

    153 Handling Errors Locally

--- react-request/src/containers/Blog/Blog.js
+++ react-request/src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,7 +24,11 @@ class Blog extends Component {
                         author: "Triss"
                     }
                 });
-                this.setState({posts: updatedPosts})
+                this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error: true});
             });
     }
 
@@ -33,14 +38,17 @@ class Blog extends Component {
 
     render() {
 
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id} title={post.title} author={post.author}
-                clicked={() => {
-                    this.postSelectedHandler(post.id)
-                }}/>
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong.</p>
 
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id} title={post.title} author={post.author}
+                    clicked={() => {
+                        this.postSelectedHandler(post.id)
+                    }}/>
+            });
+        }
         return (
             <div>
                 <section className="Posts">
