7b58e43c6b591f23be78f7e1de4db886c87af69a
torikraju <torikraju@hotmail.com>
2018-10-18 06:25:51 +0600

    125 Adding the Price to the Order Summary

--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -79,7 +79,8 @@ class BurgerBuilder extends Component {
                     <OrderSummary
                         ingredients={this.state.ingredients}
                         purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinue={this.purchaseContinueHandler}/>
+                        purchaseContinue={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
