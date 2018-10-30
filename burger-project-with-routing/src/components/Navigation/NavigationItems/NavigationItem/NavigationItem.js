604c66595c1f35efedd0d13ba1c8a926ae3cc795
torikraju <torikraju@hotmail.com>
2018-10-30 20:00:18 +0600

    210 Implementing Navigation Links

--- burger-project-with-routing/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-project-with-routing/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import styles from './NavigationItem.module.css'
+import styles from './NavigationItem.module.css';
+import {NavLink} from 'react-router-dom';
 
 const navigationItem = (props) => (
     <li className={styles.NavigationItem}>
-        <a
-            href={props.link}
-            className={props.active ? styles.active : null}>{props.children}</a>
+        <NavLink activeClassName={styles.active}
+                 exact={props.exact}
+                 to={props.link}>{props.children} </NavLink>
     </li>
 );
 
