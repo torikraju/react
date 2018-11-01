c3b8792d6e9a42e8e6f22df6de1d5bcdd4dc239a
torikraju <torikraju@hotmail.com>
2018-11-02 00:07:54 +0600

    243 Switch-Case in the Reducer

--- redux-basic/src/containers/Counter/Counter.js
+++ redux-basic/src/containers/Counter/Counter.js
@@ -5,35 +5,6 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import {connect} from 'react-redux';
 
 class Counter extends Component {
-    state = {
-        counter: 5
-    }
-
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => {
-                    return {counter: prevState.counter + 1}
-                })
-                break;
-            case 'dec':
-                this.setState((prevState) => {
-                    return {counter: prevState.counter - 1}
-                })
-                break;
-            case 'add':
-                this.setState((prevState) => {
-                    return {counter: prevState.counter + value}
-                })
-                break;
-            case 'sub':
-                this.setState((prevState) => {
-                    return {counter: prevState.counter - value}
-                })
-                break;
-        }
-    }
-
     render() {
         return (
             <div>
--- redux-basic/src/store/reducer.js
+++ redux-basic/src/store/reducer.js
@@ -3,32 +3,28 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+    switch (action.type) {
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.value
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.value
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        default:
+            return {
+                counter: state
+            }
     }
-
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.value
-        }
-    }
-
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
-    }
-
-
-    return state;
 }
 
 export default reducer;
