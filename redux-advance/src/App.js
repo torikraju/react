a6bc257b8f4db854c40bc8b797775a0437a6e693
torikraju <torikraju@hotmail.com>
2018-11-03 19:46:20 +0600

    refactoring redux-advance project

--- redux-advance/src/App.js
+++ redux-advance/src/App.js
@@ -4,13 +4,13 @@ import Counter from './containers/Counter/Counter';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <Counter />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default App;
