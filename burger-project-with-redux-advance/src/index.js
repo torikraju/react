66aeab1bc100258b5ce5b199c2fdfe6f093ec028
torikraju <torikraju@hotmail.com>
2018-11-04 16:56:37 +0600

    283 executing async code

--- burger-project-with-redux-advance/src/index.js
+++ burger-project-with-redux-advance/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
 import burgerBuilder from './store/reducers/burgerBuilder';
 
-const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(burgerBuilder, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
