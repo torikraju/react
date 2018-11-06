0dbb961a7db31e17ca08cb98f173e55cd9341f51
torikraju <torikraju@hotmail.com>
2018-11-07 00:50:31 +0600

    320 Displaying User Specific Orders

--- burger-project-with-authentication/src/App.js
+++ burger-project-with-authentication/src/App.js
@@ -17,29 +17,19 @@ class App extends Component {
     }
 
     render() {
-        let routes = (
-            <Switch>
-                <Route path="/auth" component={Auth}/>
-                <Route path="/" exact component={BurgerBuilder}/>
-                <Redirect to="/"/>
-            </Switch>
-        );
-
-        if (this.props.isAuthenticated) {
-            routes = (
-                <Switch>
-                    <Route path="/checkout" component={Checkout}/>
-                    <Route path="/orders" component={Orders}/>
-                    <Route path="/logout" component={Logout}/>
-                    <Route path="/" exact component={BurgerBuilder}/>
-                </Switch>
-            );
-        }
-        
         return (
             <div>
                 <Layout>
-                    {routes}
+                    {/*{routes}*/}
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route exact path="/orders"
+                               render={() => (this.props.isAuthenticated ? (<Orders/>) : (<Redirect to="/"/>))}/>
+                        <Route path="/logout" component={Logout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                        <Route path="/auth" component={Auth}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
                 </Layout>
             </div>
         );
--- burger-project-with-authentication/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project-with-authentication/src/containers/Checkout/ContactData/ContactData.js
@@ -62,7 +62,8 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: this.getFromData()
+            orderData: this.getFromData(),
+            userId: this.props.userId
         };
         this.props.onOrderBurger(order, this.props.token);
     };
@@ -117,7 +118,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
